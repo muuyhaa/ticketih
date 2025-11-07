c01599938cee9c345a4a6446853d6de06548bf9c
maha <elothemanymaha@gmail.com>
2025-11-08 00:33:44 +0100

    fix: Correct case sensitivity error in App.jsx import for UserDashboard

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import AuthPage from './pages/auth';
 import SuperAdminDashboard from './pages/SAdminDashboard';
 import AdminDashboard from './pages/AdminDashboard';
-import UserDashboard from './pages/userdashboard';
+import UserDashboard from './pages/UserDashboard';
 import AgentDashboard from './pages/agent';
 
 function App() {
